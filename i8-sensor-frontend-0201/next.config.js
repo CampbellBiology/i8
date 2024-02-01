50a005965e38d45b3440cf5002f8fceee76eb879
hyunha <hhko@inwoolab.com>
2024-02-01 11:44:12 +0900

    0201

---
+++ i8-sensor-frontend-0201/next.config.js
@@ -0,0 +1,27 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path')
+
+/** @type {import('next').NextConfig} */
+
+// Remove this if you're not using Fullcalendar features
+
+module.exports = {
+  trailingSlash: true,
+  reactStrictMode: false,
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `http://183.101.208.3:14530/api/:path*`,
+      },
+    ];
+  },
+  webpack: config => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      apexcharts: path.resolve(__dirname, './node_modules/apexcharts-clevision')
+    }
+
+    return config
+  }
+}
